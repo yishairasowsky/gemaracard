2d44dc506ad4dc24c492c14a02d9f7e00c114796
BenBagBag <IsaacC@allenai.org>
2017-05-13 17:46:39 -0700

    add bootstrap

--- gemaracard/gemaracard/settings.py
+++ gemaracard/gemaracard/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'bootstrap3',
 ]
 
 MIDDLEWARE = [
@@ -55,7 +56,7 @@ ROOT_URLCONF = 'gemaracard.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'gemaracard/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
