4058a27c1260de626834bad44e0ef7c6b6f7939a
BenBagBag <IsaacC@allenai.org>
2017-05-27 21:33:57 -0700

    loanword language display toggles

--- gemaracard/flashcard/forms.py
+++ gemaracard/flashcard/forms.py
@@ -11,8 +11,9 @@ class FlashcardForm(forms.ModelForm):
         ('LW', 'Loanword',)
     )
     language = forms.ChoiceField(widget=forms.Select, choices=LANGUAGE_CHOICES)
+    loanword_language = forms.CharField(widget=forms.TextInput())
     class Meta:
         model = Flashcard
-        fields = ('vocab_term','language', 'part_of_speech', 'root',
+        fields = ('vocab_term','language', 'loanword_language', 'part_of_speech', 'root',
                   'page_number', 'translation', 'notes',)
     # http://stackoverflow.com/questions/24783275/django-form-with-choices-but-also-with-freetext-option
