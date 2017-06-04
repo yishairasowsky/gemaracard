afae9e9cd9ce16ea460faa2c15a3a45f8d669299
BenBagBag <IsaacC@allenai.org>
2017-06-03 19:50:20 -0700

    tests2

--- gemaracard/flashcard/tests.py
+++ gemaracard/flashcard/tests.py
@@ -3,6 +3,8 @@ from django.test import Client, TestCase
 from django.test.client import RequestFactory
 from django.core.urlresolvers import reverse
 
+from .forms import FlashcardForm
+from .models import Flashcard
 from .views import index
 
 # to run: $ ./manage.py test
@@ -22,3 +24,12 @@ class GemaraCardTest(TestCase):
         # resp = self.client.get('/')
         resp = self.client.get(reverse('index'))
         self.assertEqual(resp.status_code, 200)
+
+    def test_init_form(self):
+        f = Flashcard.objects.all()[0]
+        form = FlashcardForm(instance=f)
+        self.assertTrue(isinstance(form.instance, Flashcard))
+
+    def test_flashcard(self):
+        flashcard = Flashcard.objects.all()[0]
+        self.assertTrue(isinstance(flashcard, Flashcard))
